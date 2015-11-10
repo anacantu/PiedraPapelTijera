cdb404524a951965e1e6751d38dc887c55eabee9
AnaCristina Cantu <anacantu1@gmail.com>
2015-11-09 21:34:28 -0600

    incluí el ciclo de ´while´en el juego

---
+++ juego3.js
@@ -0,0 +1,42 @@
+function getRandomInt (min,max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
+
+
+var respuesta = "si";
+
+while (respuesta == "si") {
+	var usuario = prompt("Escoge 1 para piedra, 2 para papel, y 3 para tijera");
+
+	var computadora = getRandomInt(0,2);
+
+	if (computadora == usuario) {
+		console.log("empate");
+	}
+
+	if (usuario == 0 && computadora == 1 ) {
+		console.log("perdiste");
+	}
+	if (usuario == 0 && computadora == 2 ) {
+		console.log("ganaste");
+	}
+	if (usuario == 1 && computadora == 0 ) {
+		console.log("ganaste");
+	}
+	if (usuario == 1 && computadora == 2 ) {
+		console.log("perdiste");
+	}
+	if (usuario == 2 && computadora == 0 ) {
+		console.log("perdiste");
+	}
+	if (usuario == 2 && computadora == 1 ) {
+		console.log("ganaste");
+	}
+	var pregunta = prompt("Quieres volver a jugar?");
+}
+
+
